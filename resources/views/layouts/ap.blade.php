8f1b7b66179c2a25a75e45136201e1caa036af6a
Mata Merah <matamerah@Matas-iMac.local>
2019-03-27 11:34:45 +0700

    remove verify

--- resources/views/layouts/ap.blade.php
+++ resources/views/layouts/ap.blade.php
@@ -94,7 +94,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.6.3/css/all.css' integrity='sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/' crossorigin='anonymous'>
 
@@ -102,7 +102,7 @@
     <script src="{{ asset('js/addons/datatables.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="https://unpkg.com/ionicons@4.4.8/dist/ionicons.js"></script>
-    
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
@@ -115,11 +115,11 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                
+
                   <a class="navbar-brand" href="{{ url('orders') }}" style="margin-top: 15px;">
                     Sanguman-master</i>
                   </a>
-               
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -149,9 +149,7 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" style="left:30px; margin:10px; height: 75PX;">
-
-                                    <a class="dropdown-item" href="{{ url('/profile') }}" ><ion-icon ios="ios-contact" md="md-contact"></ion-icon> Profil</a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown" style="left:30px; margin:10px; height: 50PX;">
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
