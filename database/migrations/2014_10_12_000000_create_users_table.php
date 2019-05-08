2ce236b305f9d123f275d4bdc69d7c3c62eb21b4
Mata Merah <matamerah@Matas-iMac.local>
2019-05-08 10:22:01 +0700

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar')->default('default.jpg');
+            $table->string('avatar')->default('default.png');
             $table->string('latar')->default('latar.jpg');
             $table->rememberToken();
             $table->timestamps();
